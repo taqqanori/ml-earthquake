9638e42f3470351ebd6be2c36ad7eff053003d78
taqqanori <taqanori@gmail.com>
2020-02-24 14:43:15 +0900

    freq scatter

--- eda.py
+++ eda.py
@@ -1,15 +1,20 @@
+import os
 import matplotlib.pyplot as plt
+from ml_earthquake.collect_data import collect_data
 from ml_earthquake.preprocess import _distance, _midnight
 import pandas as pd
 from tqdm import tqdm
 from datetime import timedelta
 
 def scatter(lat, lng, r):
+    data_path = 'data/earthquakes_all.csv'
+    if not os.path.exists(data_path):
+        collect_data(data_path, start_time='1900-01-01 00:00:00')
     x_out = []
     y_out = []
     x_in = []
     y_in = []
-    df = pd.read_csv('data/earthquakes.csv', parse_dates=['time'], index_col=0)
+    df = pd.read_csv(data_path, parse_dates=['time'], index_col=0)
     for date, row in tqdm(df.iterrows(), total=len(df)):
         if _distance(lat, lng, row['latitude'], row['longitude']) < r:
             x_in.append(date)
@@ -18,10 +23,15 @@ def scatter(lat, lng, r):
             x_out.append(date)
             y_out.append(row['mag'])
             
-    plt.scatter(x_out, y_out, s=0.1, alpha=0.1)
-    plt.scatter(x_in, y_in, s=0.1)
+    _, ax = plt.subplots()
+    ax.set_title('All Earthquakes')
+    ax.set_xlabel('date')
+    ax.set_ylabel('magnitude')
+    ax.scatter(x_out, y_out, s=0.1, alpha=0.1)
+    ax.scatter(x_in, y_in, s=0.1, label='Around Tokyo 150km')
+    ax.legend(loc='upper left')
 
-    plt.show()
+    plt.savefig('img/frequency.png')
 
 def count(lat, lng, r, mag):
     p = 0
