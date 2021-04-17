67ba63ba715a005709cb52cd2b335af5035d4c65
taqqanori <taqanori@gmail.com>
2021-04-17 13:46:33 +0900

    AUC 0.6

--- ml_earthquake/train.py
+++ ml_earthquake/train.py
@@ -1,4 +1,5 @@
-import os, json
+import os
+import json
 import numpy as np
 import random as rn
 import tensorflow as tf
@@ -7,6 +8,7 @@ from sklearn.metrics import confusion_matrix, roc_auc_score
 from keras.callbacks import Callback
 from keras.models import Sequential, load_model
 from keras.layers.convolutional_recurrent import ConvLSTM2D
+from keras.layers import Conv3D
 from keras.layers.normalization import BatchNormalization
 from keras.optimizers import Adam
 from keras.layers.core import Dense, Activation, Dropout, Flatten
@@ -27,31 +29,55 @@ from imblearn.keras import BalancedBatchGenerator
 
 date_format = '%Y%m%d'
 
+
 def train(
-    X_train,
-    y_train,
-    X_test,
-    y_test,
-    info_train=None,
-    info_test=None,
-    out_dir=None,
-    model_file_name='best_model.h5',
-    learning_rate=5e-6,
-    decay=0.0,
-    epochs=100,
-    dropout=0.3,
-    log_dir=None,
-    resampling_methods=None,
-    balanced_batch=None,
-    use_class_weight=False,
-    random_state=4126):
+        X_train,
+        y_train,
+        X_test,
+        y_test,
+        info_train=None,
+        info_test=None,
+        out_dir=None,
+        model_file_name='best_model.h5',
+        learning_rate=5e-6,
+        decay=0.0,
+        epochs=100,
+        dropout=0.3,
+        log_dir=None,
+        resampling_methods=None,
+        balanced_batch=None,
+        use_class_weight=False,
+        random_state=4126):
     if out_dir is not None:
         os.makedirs(out_dir, exist_ok=True)
 
     model = Sequential()
 
     model.add(ConvLSTM2D(
-        input_shape=(X_train.shape[1], X_train.shape[2], X_train.shape[3], X_train.shape[4]),
+        input_shape=(X_train.shape[1], X_train.shape[2],
+                     X_train.shape[3], X_train.shape[4]),
+        filters=30,
+        kernel_size=(3, 3),
+        padding='same',
+        dropout=dropout,
+        return_sequences=True
+    ))
+    model.add(BatchNormalization())
+
+    model.add(ConvLSTM2D(
+        input_shape=(X_train.shape[1] - 2, X_train.shape[2] -
+                     2, X_train.shape[3] - 2, X_train.shape[4]),
+        filters=30,
+        kernel_size=(3, 3),
+        padding='same',
+        dropout=dropout,
+        return_sequences=True
+    ))
+    model.add(BatchNormalization())
+
+    model.add(ConvLSTM2D(
+        input_shape=(X_train.shape[1] - 4, X_train.shape[2] -
+                     4, X_train.shape[3] - 4, X_train.shape[4]),
         filters=30,
         kernel_size=(3, 3),
         padding='same',
@@ -60,15 +86,99 @@ def train(
     ))
     model.add(BatchNormalization())
 
+    # filters = 10
+    # kernel_size = 3
+    # model.add(Conv3D(
+    #     filters,
+    #     kernel_size,
+    #     activation='relu',
+    #     input_shape=(X_train.shape[1], X_train.shape[2],
+    #                  X_train.shape[3], X_train.shape[4]),
+    # ))
+    # model.add(BatchNormalization())
+
+    # model.add(Conv3D(
+    #     filters,
+    #     kernel_size,
+    #     activation='relu',
+    #     input_shape=(X_train.shape[1] - (kernel_size - 1), X_train.shape[2] - (
+    #         kernel_size - 1), X_train.shape[3] - (kernel_size - 1), filters),
+    # ))
+    # model.add(BatchNormalization())
+
+    # model.add(Conv3D(
+    #     filters,
+    #     kernel_size,
+    #     activation='relu',
+    #     input_shape=(X_train.shape[1] - 2 * (kernel_size - 1), X_train.shape[2] - 2 * (
+    #         kernel_size - 1), X_train.shape[3] - 2 * (kernel_size - 1), filters),
+    # ))
+    # model.add(BatchNormalization())
+
+    # model.add(Conv3D(
+    #     filters,
+    #     kernel_size,
+    #     activation='relu',
+    #     input_shape=(X_train.shape[1] - 3 * (kernel_size - 1), X_train.shape[2] - 3 * (
+    #         kernel_size - 1), X_train.shape[3] - 3 * (kernel_size - 1), filters),
+    # ))
+    # model.add(BatchNormalization())
+
+    # model.add(Conv3D(
+    #     filters,
+    #     kernel_size,
+    #     activation='relu',
+    #     input_shape=(X_train.shape[1] - 4 * (kernel_size - 1), X_train.shape[2] - 4 * (
+    #         kernel_size - 1), X_train.shape[3] - 4 * (kernel_size - 1), filters),
+    # ))
+    # model.add(BatchNormalization())
+
+    # model.add(Conv3D(
+    #     filters,
+    #     kernel_size,
+    #     activation='relu',
+    #     input_shape=(X_train.shape[1] - 5 * (kernel_size - 1), X_train.shape[2] - 5 * (
+    #         kernel_size - 1), X_train.shape[3] - 5 * (kernel_size - 1), filters),
+    # ))
+    # model.add(BatchNormalization())
+
+    # model.add(Conv3D(
+    #     filters,
+    #     kernel_size,
+    #     activation='relu',
+    #     input_shape=(X_train.shape[1] - 6 * (kernel_size - 1), X_train.shape[2] - 6 * (
+    #         kernel_size - 1), X_train.shape[3] - 6 * (kernel_size - 1), filters),
+    # ))
+    # model.add(BatchNormalization())
+
     model.add(Flatten())
 
+    model.add(Dense(5000, activation='relu'))
+    model.add(Dropout(dropout))
+
+    model.add(Dense(3000, activation='relu'))
+    model.add(Dropout(dropout))
+
+    model.add(Dense(1000, activation='relu'))
+    model.add(Dropout(dropout))
+
+    model.add(Dense(500, activation='relu'))
+    model.add(Dropout(dropout))
+
+    model.add(Dense(300, activation='relu'))
+    model.add(Dropout(dropout))
+
     model.add(Dense(100, activation='relu'))
+    model.add(Dropout(dropout))
+
+    model.add(Dense(10, activation='relu'))
     model.add(Dropout(dropout))
 
     model.add(Dense(1, activation='sigmoid'))
 
     adam = Adam(lr=learning_rate, decay=decay)
-    model.compile(optimizer=adam, loss='binary_crossentropy', metrics=["accuracy"])
+    model.compile(optimizer=adam, loss='binary_crossentropy',
+                  metrics=["accuracy"])
     model.summary()
 
     reporter = _Reporter(X_test, y_test)
@@ -78,13 +188,15 @@ def train(
     if out_dir is not None:
         model_path = os.path.join(out_dir, model_file_name)
         callbacks.append(
-            ModelCheckpoint(filepath=model_path, verbose=1, save_best_only=True)
+            ModelCheckpoint(filepath=model_path,
+                            verbose=1, save_best_only=True)
         )
 
     if log_dir is not None:
-        log_path = os.path.join(log_dir, datetime.now().strftime(date_format + '_%H%M%S'))
+        log_path = os.path.join(
+            log_dir, datetime.now().strftime(date_format + '_%H%M%S'))
         callbacks.append(TensorBoard(log_dir=log_path))
-    
+
     positive = (0.5 <= y_train).sum()
     negative = (y_train < 0.5).sum()
     print('train data balance P:{} : N:{}'.format(positive, negative))
@@ -95,29 +207,33 @@ def train(
 
     if resampling_methods is not None:
         for resampling_method in resampling_methods:
-            X_train, y_train = _resample(X_train, y_train, resampling_method, random_state)
+            X_train, y_train = _resample(
+                X_train, y_train, resampling_method, random_state)
 
     if balanced_batch == True:
         print('using BalancedBatchGenerator')
-        batch_gen = BalancedBatchGenerator(\
-            X_train, y_train, \
+        batch_gen = BalancedBatchGenerator(
+            X_train, y_train,
             sampler=RandomUnderSamplerWrapper(), random_state=random_state)
-        model.fit_generator(generator=batch_gen, \
-            epochs=epochs, callbacks=callbacks, \
-            validation_data=(X_test, y_test),
-            class_weight=class_weight)
+        model.fit_generator(generator=batch_gen,
+                            epochs=epochs, callbacks=callbacks,
+                            validation_data=(X_test, y_test),
+                            class_weight=class_weight)
     else:
-        model.fit(X_train, y_train, \
-            epochs=epochs, callbacks=callbacks, \
-            validation_data=(X_test, y_test),
-            class_weight=class_weight)
-    
+        model.fit(X_train, y_train,
+                  epochs=epochs, callbacks=callbacks,
+                  validation_data=(X_test, y_test),
+                  class_weight=class_weight)
+
     if out_dir is not None and info_train is not None and info_test is not None:
-        _output(out_dir, X_test, y_test, info_train, info_test, model_path, reporter)
-    
+        _output(out_dir, X_test, y_test, info_train,
+                info_test, model_path, reporter)
+
+
 def _output(out_dir, X_test, y_test, info_train, info_test, model_path, reporter):
     best_model = load_model(model_path)
-    acc, auc, f1, precision, recall, tp, fn, fp, tn = _eval(best_model, X_test, y_test)
+    acc, auc, f1, precision, recall, tp, fn, fp, tn = _eval(
+        best_model, X_test, y_test)
 
     # summary
     summary = {
@@ -222,23 +338,27 @@ def _output(out_dir, X_test, y_test, info_train, info_test, model_path, reporter
                 max_mag = eq['mag']
         validation['max_mag'] = max_mag
         _dump(detail, os.path.join(out_dir, detail_path))
-    _dump({ 'validations': validations }, os.path.join(out_dir, 'validations.json'))
+    _dump({'validations': validations},
+          os.path.join(out_dir, 'validations.json'))
+
 
 def _resample(X_train, y_train, resampling_method, random_state):
     resampler = _get_resampler(resampling_method, random_state)
     print('performing {}...'.format(resampling_method['name']))
-    X_train_resample, y_train = resampler.fit_sample(X_train.reshape(X_train.shape[0], -1), y_train)
-    X_train = X_train_resample.reshape(\
-        X_train_resample.shape[0], \
-        X_train.shape[1], \
-        X_train.shape[2], \
-        X_train.shape[3], \
+    X_train_resample, y_train = resampler.fit_sample(
+        X_train.reshape(X_train.shape[0], -1), y_train)
+    X_train = X_train_resample.reshape(
+        X_train_resample.shape[0],
+        X_train.shape[1],
+        X_train.shape[2],
+        X_train.shape[3],
         X_train.shape[4])
     positive = (0.5 <= y_train).sum()
     negative = (y_train < 0.5).sum()
     print('{} performed train data balance P:{} : N:{}'.format(
         resampling_method['name'], positive, negative))
     return X_train, y_train
+
 
 def _get_resampler(method, random_state, n_jobs=8):
     args = {
@@ -254,7 +374,7 @@ def _get_resampler(method, random_state, n_jobs=8):
         return SMOTE(**args)
     elif method['name'] == 'RandomUnderSampler':
         del args['n_jobs']
-        return RandomUnderSampler(**args) # pylint: disable=unexpected-keyword-arg
+        return RandomUnderSampler(**args)  # pylint: disable=unexpected-keyword-arg
     elif method['name'] == 'ClusterCentroids':
         return ClusterCentroids(**args)
     elif method['name'] == 'NearMiss':
@@ -272,21 +392,24 @@ def _get_resampler(method, random_state, n_jobs=8):
     else:
         raise Exception('unknown resampler: {}'.format(method['name']))
 
+
 class RandomUnderSamplerWrapper(RandomUnderSampler):
 
     def fit_resample(self, X, y):
         X_resample, y = super().fit_sample(X.reshape(X.shape[0], -1), y)
-        X = X_resample.reshape(\
-            X_resample.shape[0], \
-            X.shape[1], \
-            X.shape[2], \
-            X.shape[3], \
+        X = X_resample.reshape(
+            X_resample.shape[0],
+            X.shape[1],
+            X.shape[2],
+            X.shape[3],
             X.shape[4])
         return X, y
+
 
 def _dump(o, path):
     with open(path, 'w') as f:
         json.dump(o, f)
+
 
 class _Reporter(Callback):
 
@@ -311,10 +434,12 @@ class _Reporter(Callback):
             if self.best < score:
                 return
             self.best = score
-        acc, auc, f1, precision, recall, tp, fn, fp, tn = _eval(self.model, self.X_test, self.y_test)
-        print('Epoch {}: AUC: {:.3f}, F1: {:.3f}, acc: {:.3f}, precision: {:.3f}, recall: {:.3f}, TP: {}, FN: {}, FP: {}, TN: {}'.format(\
+        acc, auc, f1, precision, recall, tp, fn, fp, tn = _eval(
+            self.model, self.X_test, self.y_test)
+        print('Epoch {}: AUC: {:.3f}, F1: {:.3f}, acc: {:.3f}, precision: {:.3f}, recall: {:.3f}, TP: {}, FN: {}, FP: {}, TN: {}'.format(
             epoch + 1, auc, f1, acc, precision, recall, tp, fn, fp, tn
         ))
+
 
 def _eval(model, X_test, y_test):
     y_pred = model.predict(X_test).reshape(-1)
@@ -322,6 +447,6 @@ def _eval(model, X_test, y_test):
     auc = roc_auc_score(y_test, y_pred)
     f1 = (2 * tp) / (2 * tp + fp + fn)
     acc = (tp + tn) / (tp + tn + fp + fn)
-    precision = tp /(tp + fp)
+    precision = tp / (tp + fp)
     recall = tp / (tp + fn)
     return acc, auc, f1, precision, recall, tp, fn, fp, tn
